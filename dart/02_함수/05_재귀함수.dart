1a5eb7c4424837748d23746d37aede59d6223761
Lee-0210 <tjsans9069@gmail.com>
2025-08-28 17:56:30 +0900

    함수까지 完

---
+++ dart/02_함수/05_재귀함수.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+int factorial(int n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+void main(List<String> args) {
+  stdout.write('N : ');
+  String? input = stdin.readLineSync();
+  print(input);
+  int N = int.parse(input!);
+  print(factorial(N));
+}
